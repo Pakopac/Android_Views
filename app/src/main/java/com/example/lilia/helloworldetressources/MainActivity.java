df7cf3e55f01b33f9a777c68e993b6e9a7c92ccc
Pakopac <lilian.pacaud@supinternet>
2018-12-06 12:04:17 +0100

    Changed textView to Button for button shopping list

--- app/src/main/java/com/example/lilia/helloworldetressources/MainActivity.java
+++ app/src/main/java/com/example/lilia/helloworldetressources/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.view2);
+        setContentView(R.layout.view1);
     }
 
 
